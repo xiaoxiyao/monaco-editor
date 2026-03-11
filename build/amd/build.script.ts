dcffd7bcfafef4b121dd8a85b7977e80622b0cf8
xiaoxiyao <xiyao.luoxiao@gmail.com>
2026-03-11 16:56:39 +0800

    fix: 禁用开发版本构建以减少包体积

--- build/amd/build.script.ts
+++ build/amd/build.script.ts
@@ -2,7 +2,8 @@ import { run } from '../../scripts/lib/index';
 
 export async function buildAmdMinDev() {
 	const rootPath = __dirname;
-	await run('npx vite build --mode development', { cwd: rootPath });
+	// 不构建dev版本，减少包体积
+	// await run('npx vite build --mode development', { cwd: rootPath });
 	await run('npx vite build', { cwd: rootPath });
 	await run('npx rollup -c rollup-types.config.mjs', { cwd: rootPath });
 }
